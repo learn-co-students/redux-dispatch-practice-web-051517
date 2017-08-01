7217298ffa9913eca7749cd2d7c34800e7ec186a
Jan Phillip Carreon <janphillipcarreon@yahoo.com>
2017-08-01 16:55:15 -0400

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,36 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state={pets:[]}, action){
+  switch(action.type){
+    case 'ADD_PET':
+    state = {
+      pets:[
+        ...state.pets, action.pet
+      ]
+    }
+    break;
+
+    case 'REMOVE_PET':
+    const pets = state.pets.filter((pet)=>pet.id !== action.id)
+    state = {
+      pets
+    }
+    break;
+  }
+  return state;
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action);
+  render();
 }
 
 export function render(){
-
+  console.log(state)
+  let container = document.getElementById('container')
+  const pets = state.pets.map((pet)=>{
+    return `<li>${pet.name}</li>`
+  }).join('')
+  container.innerHTML = `<ul>${pets}</ul>`
 }
